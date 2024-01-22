6977cb157196fd58cba45b01d13071f93a0dec77
Jefferson Richards <jefferson.richards@outlook.com>
2024-01-21 22:16:11 -0300

    feat: created microservice-client

---
+++ ms-client/src/main/java/io/github/im2back/msclient/MsClientApplication.java
@@ -0,0 +1,13 @@
+package io.github.im2back.msclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClientApplication.class, args);
+	}
+
+}
