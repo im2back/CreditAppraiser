81a99f56bd98813190a93e4da6514d55698b2bb7
Jefferson Richards <jefferson.richards@outlook.com>
2024-05-21 10:32:13 -0300

    docs: Ms-client Documentation Complete

--- ms-client/src/main/java/io/github/im2back/msclient/controller/ClientController.java
+++ ms-client/src/main/java/io/github/im2back/msclient/controller/ClientController.java
@@ -18,6 +18,9 @@ import org.springframework.web.util.UriComponentsBuilder;
 import io.github.im2back.msclient.model.ClientRequestDto;
 import io.github.im2back.msclient.model.ClientResponseDto;
 import io.github.im2back.msclient.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -33,7 +36,13 @@ public class ClientController {
 		log.info("obtendo status do microservice de clientes");
 		return "ok";
 	}
-
+	
+	@Operation(summary = "Salva um Client no banco de dados e retorna um DTO do Client salvo")
+	@ApiResponses({
+		@ApiResponse(responseCode = "201", description = "Retorna um DTO de Client em caso de sucesso"),
+		@ApiResponse(responseCode = "400", description = "Retorna uma exceção tratada, lançada apartir do Bean Validation"
+				+ "uma exceção tratata"),
+		@ApiResponse(responseCode = "409", description = "Retorna uma exceção lançada pela classe ServiceValidationsExceptions")})
 	@PostMapping
 	ResponseEntity<ClientResponseDto> saveClient(@RequestBody @Valid ClientRequestDto clientRequestDto, UriComponentsBuilder uriBuilder){
 		ClientResponseDto clientResponseDto = service.saveClient(clientRequestDto);
@@ -44,7 +53,13 @@ public class ClientController {
 		return ResponseEntity.created(location).body(clientResponseDto);		
 	}
 	
-
+	
+	@Operation
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "Retorna um DTO de Client aparit de um CPF"),
+		@ApiResponse(responseCode = "400", description = "Retorna uma exceção personalizada do tipo ServiceClientExceptions"
+				+ "caso o CPF não exista")
+	})
 	@GetMapping(params="cpf")
 	ResponseEntity<ClientResponseDto> getClientByCpf(@RequestParam("cpf")  String cpf){
 		ClientResponseDto clientResponseDto = service.findByCpf(cpf);	
